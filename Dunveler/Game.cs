bc86adc5e98004f0577988c26b7cb882e4c8b8ec
GGshcka <ztorney@mail.ru>
2024-02-19 13:13:43 +0300

    First

---
+++ Dunveler/Game.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Numerics;
+using Dunveler.Resources;
+using Raylib_cs;
+
+namespace Dunveler;
+
+class Game
+{
+    public static void Main()
+    {
+        Resources.Resources.Culture = CultureInfo.GetCultureInfo("ru");
+        Raylib.InitWindow(800, 480, "Dunveler - " + Resources.Resources.splashText);
+        Raylib.SetTargetFPS(60);
+        //Raylib.SetWindowIcon(icon);
+
+        Font font = Raylib.LoadFont("Resources/Fonts/pixelcyr_normal.ttf");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.SkyBlue);
+
+            Raylib.DrawTextEx(font, Resources.Resources.helloWorldText, new Vector2(10,10), 40, 10, Color.White);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
